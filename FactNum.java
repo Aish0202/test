3f5162aa3ad688fdf2bdcff63914b54182ef771a
Aish0202 <161601915+Aish0202@users.noreply.github.com>
2024-09-20 13:36:39 +0530

    Add files via upload

---
+++ FactNum.java
@@ -0,0 +1,27 @@
+package java;
+
+import java.util.Scanner;
+
+public class FactNum {
+	public static void main(String args[])
+	{
+	 Scanner s = new Scanner(System.in);
+	 System.out.println("Enter the number");
+	 int num = s.nextInt();
+	 int n;
+	 for (int i = 1; int i+1)
+	 {
+
+		    if (n % n == 0)
+		        n += 1;
+		    n = n - 2;
+		        
+		    
+		 {
+		 System.out.println("Factorial of:"+n);
+		 
+		 }
+	 }}
+	
+
+}
---
+++ HashMap.java
@@ -0,0 +1,18 @@
+package departments;
+
+public class HashMap {
+	public static void main(String args[])
+	{
+	
+	String emp_name;
+
+	{
+	
+	System.out.println("Mesa:Pooja, geeta,Vaibhav");
+	System.out.println("EJ:Megha,Shalini,Mira");
+	System.out.println("CE:Suraj,Abhi,Ankita");
+	}
+	
+	
+	}
+}
